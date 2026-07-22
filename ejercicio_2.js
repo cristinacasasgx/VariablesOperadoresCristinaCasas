5fb7e1402893362aafd094f96fd39a7e3612e01c
Cristinacasasgx <Cristinacgalvisx@gmail.com>
2026-07-22 15:32:15 -0500

    Ejercicio 1 completado

---
+++ ejercicio_2.js
@@ -0,0 +1,7 @@
+// Lea dos números y calcule el resultado de su suma, resta, multiplicación y división.
+
+function areaRectangulo(base, altura) {
+    return base * altura;
+}
+
+console.log(areaRectangulo(12, 8)); 
