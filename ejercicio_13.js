5aefc45b9a5730e1c87fc3bcdc13ab9df4425877
Cristinacasasgx <Cristinacgalvisx@gmail.com>
2026-07-23 23:17:04 -0500

    Ejercicio 13 completado

---
+++ ejercicio_13.js
@@ -0,0 +1,6 @@
+//Dada las horas trabajadas de una persona y el valor por hora. Calcular su salario e imprimirlo. 
+
+function salarioPorHoras(horas, valorHora) {
+    return horas * valorHora;
+}
+console.log("Juan ganó " + salarioPorHoras(42, 4500) + " pesos trabajando por horas ");
